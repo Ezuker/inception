a9924185eff643981dc48d7fabace0933893eb1d
Ezuker <benoit.carolle.pro@gmail.com>
2024-08-29 14:52:50 -0400

    feat: adding adminer but nginx proxy doesnt work

--- srcs/requirements/proftpd/tools/proftpd.sh
+++ srcs/requirements/proftpd/tools/proftpd.sh
@@ -6,11 +6,4 @@ echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 chown -R $FTP_USER:$FTP_USER /var/www/wordpress
 chmod -R 755 /var/www/wordpress
 
-if ! pgrep -x "proftpd" > /dev/null
-then
-    # Start the ProFTPD server in the foreground with debug logging
-    /usr/sbin/proftpd -n -d 10
-    echo "ProFTPD has been started."
-else
-    echo "ProFTPD is already running."
-fi
+/usr/sbin/proftpd -n
